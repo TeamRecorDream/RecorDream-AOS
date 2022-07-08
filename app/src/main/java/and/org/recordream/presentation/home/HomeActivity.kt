cbe7f4c4a2e0b3f57ebdddbc099d5ec8d6000108
s9hn <tnghqn@naver.com>
2022-07-08 19:06:08 +0900

    [Chore] 기초 세팅 중
    1. navigation component추가

--- app/src/main/java/and/org/recordream/presentation/home/HomeActivity.kt
+++ app/src/main/java/and/org/recordream/presentation/home/HomeActivity.kt
@@ -1,12 +1,12 @@
-package and.org.recordream.presentation
+package and.org.recordream.presentation.home
 
 import and.org.recordream.R
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 }
