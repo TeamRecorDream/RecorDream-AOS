9cd0e17052db0dc2d5a825edd2598dc9b62f3b19
s9hn <tnghqn@naver.com>
2022-07-11 01:26:04 +0900

    [Chore] 기초 세팅 중
    1. 커스텀 바텀네비 구현

--- app/src/main/java/and/org/recordream/util/CustomBottomNavigation.kt
+++ app/src/main/java/and/org/recordream/util/CustomBottomNavigation.kt
@@ -51,10 +51,6 @@ class CustomBottomNavigationView : BottomNavigationView {
         setBackgroundColor(Color.TRANSPARENT)
     }
 
-    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
-        super.onLayout(changed, left, top, right, bottom)
-    }
-
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
 
