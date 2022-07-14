cab8b3181105bb9ffa68a9c481534840899a1e11
s9hn <tnghqn@naver.com>
2022-07-15 03:00:44 +0900

    [Feat] 보관함 뷰

--- app/src/main/java/and/org/recordream/presentation/storage/StorageFragment.kt
+++ app/src/main/java/and/org/recordream/presentation/storage/StorageFragment.kt
@@ -1,20 +1,32 @@
 package and.org.recordream.presentation.storage
 
 import and.org.recordream.R
+import and.org.recordream.databinding.FragmentStorageBinding
+import android.content.Context
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 
 class StorageFragment : Fragment() {
+    private var _binding: FragmentStorageBinding? = null
+    private val binding get() = _binding ?: error("Binding is not initialization")
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_storage, container, false)
+        _binding = FragmentStorageBinding.inflate(layoutInflater, container, false)
+
+
+        context?.let { ContextCompat.getDrawable(it,R.drawable.logo) }
+            ?.let { binding.tvStorageMyemotion.background = it }
+
+
+        return binding.root
     }
 }
