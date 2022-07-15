a961d06d03264481df1701e721260c997c3f099a
yoon <cbj0010@naver.com>
2022-07-16 05:53:08 +0900

    [Feat] #15 기록뷰 xml 작업 중간커밋

--- app/src/main/java/and/org/recordream/presentation/mypage/MypageAccountDeleteFragment.kt
+++ app/src/main/java/and/org/recordream/presentation/mypage/MypageAccountDeleteFragment.kt
@@ -1,19 +1,15 @@
 package and.org.recordream.presentation.mypage
 
-import android.app.Dialog
 import android.app.TimePickerDialog
-import android.os.Bundle
-import android.text.format.DateFormat.is24HourFormat
 import android.widget.TimePicker
 import androidx.fragment.app.DialogFragment
-import java.text.DateFormat
-import java.util.*
 
 
 class MypageAccountDeleteFragment : DialogFragment() {
 
     class TimePickerFragment : DialogFragment(), TimePickerDialog.OnTimeSetListener {
 
+        /*
         override fun onCreateDialog(savedInstanceState: Bundle): Dialog {
             // Use the current time as the default values for the picker
             val c = Calendar.getInstance()
@@ -27,9 +23,9 @@ class MypageAccountDeleteFragment : DialogFragment() {
                 hour,
                 minute,
                 DateFormat.is24HourFormat(activity)
-            )
+//            )
         }
-
+*/
         override fun onTimeSet(view: TimePicker, hourOfDay: Int, minute: Int) {
             // Do something with the time chosen by the user
         }
