f03c9912c73d3803fd5aa4ef26700f257ea71bdf
s9hn <tnghqn@naver.com>
2022-07-18 04:11:55 +0900

    [Feat] 보관함 뷰

--- app/src/main/java/and/org/recordream/presentation/storage/myrecord/ListTypeFragment.kt
+++ app/src/main/java/and/org/recordream/presentation/storage/myrecord/ListTypeFragment.kt
@@ -1,21 +1,29 @@
 package and.org.recordream.presentation.storage.myrecord
 
+import and.org.recordream.R
+import and.org.recordream.databinding.FragmentListTypeBinding
+import and.org.recordream.databinding.FragmentStorageBinding
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import and.org.recordream.R
+import androidx.fragment.app.Fragment
 
 
 class ListTypeFragment : Fragment() {
+    private var _binding: FragmentListTypeBinding? = null
+    private val binding get() = _binding ?: error("Binding is not initialization")
+    private lateinit var listTypeAdapter: ListTypeAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list_type, container, false)
+        _binding = FragmentListTypeBinding.inflate(layoutInflater, container, false)
+
+
+        // 어댑터 연동
+        return binding.root
     }
 
 }
