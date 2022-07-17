35e04c3e4d34d9ede0f10a9b39292877a93b993c
s9hn <tnghqn@naver.com>
2022-07-18 03:22:09 +0900

    [Feat] 보관함 뷰
    1. galleryTypeFragment rename

--- app/src/main/java/and/org/recordream/presentation/storage/myrecord/GalleryTypeFragment.kt
+++ app/src/main/java/and/org/recordream/presentation/storage/myrecord/GalleryTypeFragment.kt
@@ -7,14 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class GalleryFragment : Fragment() {
+class GalleryTypeFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gallery, container, false)
+        return inflater.inflate(R.layout.fragment_gallery_type, container, false)
     }
 
 }
