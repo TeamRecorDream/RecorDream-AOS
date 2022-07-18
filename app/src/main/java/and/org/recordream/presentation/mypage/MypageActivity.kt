2b204328b4772b7a18b9cbfd1b57c0ef64aff116
yoon <cbj0010@naver.com>
2022-07-18 14:47:18 +0900

    [Chore] 에러수정중

--- app/src/main/java/and/org/recordream/presentation/mypage/MypageActivity.kt
+++ app/src/main/java/and/org/recordream/presentation/mypage/MypageActivity.kt
@@ -3,15 +3,13 @@ package and.org.recordream.presentation.mypage
 import and.org.recordream.R
 import and.org.recordream.databinding.ActivityMypageBinding
 import and.org.recordream.util.CustomDialog
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class MypageActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMypageBinding
     private lateinit var dialog: CustomDialog
+    private val myPageBottomSheetFragment = MypageBottomSheetFragment()
 
     var isSwitch: Boolean = false
 
@@ -19,17 +17,19 @@ class MypageActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMypageBinding.inflate(layoutInflater)
 
-        //setOnDreamPushClickEvent()
-        //initBottomSheet()
-        showDialog()
         setContentView(binding.root)
-    }
+        binding.scMypageSwitchbtn.setOnClickListener {
 
-    private fun setonNickNameClick() {
-        binding.tvMypageNickname.setOnClickListener {
-
+            createBottomSheet()
         }
+
+        showDialog()
     }
+
+    private fun createBottomSheet() {
+        myPageBottomSheetFragment.show(supportFragmentManager, myPageBottomSheetFragment.tag)
+    }
+
 
     private fun showDialog() {
         binding.tvMypageWithdrawl.setOnClickListener {
@@ -37,29 +37,6 @@ class MypageActivity : AppCompatActivity() {
             dialog.showDeleteDialog(R.layout.detail_delete_dialog)
         }
     }
-
-//    private fun initBottomSheet() {
-//        btnShowBottomSheet.setOnClickListener {
-//
-//            // on below line we are creating a new bottom sheet dialog.
-//            val dialog = BottomSheetDialog(this)
-//
-//            //커on below line we are inflating a layout file which we have created.
-//            val view = layoutInflater.inflate(R.layout.detail_bottom_sheet, null)
-//            //배경 투명하게 해줘서 커스텀한 모양이 보이게
-//            dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-//            //외부 화면 누르면 창 자동으로 닫히게
-//            dialog.setCancelable(true)
-//
-//            // on below line we are setting
-//            // content view to our view.
-//            dialog.setContentView(view)
-//
-//            // on below line we are calling
-//            // a show method to display a dialog.
-//            dialog.show()
-//        }
-
 }
 
 
--- app/src/main/java/and/org/recordream/presentation/mypage/MypageBottomSheetFragment.kt
+++ app/src/main/java/and/org/recordream/presentation/mypage/MypageBottomSheetFragment.kt
@@ -1,24 +1,33 @@
 package and.org.recordream.presentation.mypage//package before.forget.feature.write
 
+import and.org.recordream.R
 import and.org.recordream.databinding.FragmentMypageBottomSheetBinding
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContentProviderCompat
+import androidx.databinding.DataBindingUtil
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
-class WriteBottomSheetFragment : BottomSheetDialogFragment() {
-    private var _binding: FragmentMypageBottomSheetBinding? = null
-    private val binding get() = _binding ?: error("바인딩에 NULL 값이 들어갔어요!!")
+class MypageBottomSheetFragment : BottomSheetDialogFragment() {
+    private lateinit var binding: FragmentMypageBottomSheetBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentMypageBottomSheetBinding.inflate(inflater, container, false)
-        amOrpmSettiing()
-
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_mypage_bottom_sheet,
+            container,
+            false
+        )
+         amOrpmSettiing()
+        initDialog()
         return binding.root
     }
 
@@ -35,53 +44,12 @@ class WriteBottomSheetFragment : BottomSheetDialogFragment() {
             amOrpm = str[i]
         }
     }
-}
 
-//        binding.btnWriteApply.visibility = View.GONE
-//        binding.clWriteResetbtn.visibility = View.GONE
-//
-//        /*  writeBadFragment.setCallbackButtonClickListener {
-//              dismiss()
-//          }*/
-//
-//        initAdapter()
-//        initTabLayout()
-//        test()
-//        initDialog()
-//        writeGoodFragment.setCallbackButtonClickListener { oneLine ->
-//            oneLineCallback?.invoke(oneLine)
-//            Log.d("액비티티에서", "오긴온거니 흑")
-//            dismiss()
-//        }
-//
-//        return binding.root
-//    }
-//
-//    private fun initAdapter() { // 뷰페이저 어댑터
-//        val fragmentList = listOf(writeGoodFragment, writeBadFragment)
-//        writeViewPagerAdapter = WriteViewPagerAdapter(this)
-//        writeViewPagerAdapter.fragments.addAll(fragmentList)
-//        binding.vpWriteMenu.adapter = writeViewPagerAdapter
-//    }
-//
-//    private fun initTabLayout() { // 탭레이아웃
-//        val menuList = listOf("좋았어요", "아쉬워요")
-//        TabLayoutMediator(binding.tlWriteMenu, binding.vpWriteMenu) { tab, position ->
-//            tab.text = menuList[position]
-//        }.attach()
-//    }
-//
-//    private fun test() {
-//        binding.btnWriteApply.setOnClickListener {
-//            val writeBottomSheetFragment = WriteBottomSheetFragment()
-//            writeBottomSheetFragment.dismiss()
-//            // 데이터전달
-//        }
-//    }
-//
-//    private fun initDialog() { // 바텀시트달기
-//        val bottomSheetDialog = BottomSheetDialog(requireContext())
-//        bottomSheetDialog.behavior.state = BottomSheetBehavior.STATE_COLLAPSED
-//        bottomSheetDialog.behavior.state = BottomSheetBehavior.STATE_EXPANDED
-//    }
+    private fun initDialog() { // 바텀시트달기
+        val bottomSheetDialog = BottomSheetDialog(requireContext())
+        bottomSheetDialog.behavior.state = BottomSheetBehavior.STATE_COLLAPSED
+        bottomSheetDialog.behavior.state = BottomSheetBehavior.STATE_EXPANDED
+    }
+
+}
 
