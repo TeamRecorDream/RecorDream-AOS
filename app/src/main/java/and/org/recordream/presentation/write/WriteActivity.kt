5ac4fffc14510f90cbdba33da3cd7322df5bd5dc
yoon <cbj0010@naver.com>
2022-07-19 23:56:50 +0900

    [Feat] write 꿈select 생!성

--- app/src/main/java/and/org/recordream/presentation/write/WriteActivity.kt
+++ app/src/main/java/and/org/recordream/presentation/write/WriteActivity.kt
@@ -3,9 +3,6 @@ package and.org.recordream.presentation.write
 import and.org.recordream.databinding.ActivityWriteBinding
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
-import java.util.*
 
 class WriteActivity : AppCompatActivity() {
     private lateinit var binding: ActivityWriteBinding
@@ -16,6 +13,7 @@ class WriteActivity : AppCompatActivity() {
         binding = ActivityWriteBinding.inflate(layoutInflater)
 
         voiceClick()
+        emotionClick()
         setContentView(binding.root)
     }
 
@@ -23,10 +21,116 @@ class WriteActivity : AppCompatActivity() {
         voiceFragment.show(supportFragmentManager, voiceFragment.tag)
     }
 
-    private fun voiceClick(){
+    private fun voiceClick() {
         binding.clWriteVoiceRecord.setOnClickListener {
             createBottomSheet()
         }
     }
 
+    private fun emotionClick() {
+        with(binding) {
+            ivEmotionJoy.setOnClickListener {
+                ivEmotionJoy.isSelected = true
+                ivEmotionShocked.isSelected = false
+                ivEmotionLove.isSelected = false
+                ivEmotionShy.isSelected = false
+                ivEmotionSad.isSelected = false
+                ivEmotionAngry.isSelected = false
+            }
+            ivEmotionShocked.setOnClickListener {
+                ivEmotionJoy.isSelected = false
+                ivEmotionShocked.isSelected = true
+                ivEmotionLove.isSelected = false
+                ivEmotionShy.isSelected = false
+                ivEmotionSad.isSelected = false
+                ivEmotionAngry.isSelected = false
+            }
+            ivEmotionLove.setOnClickListener {
+                ivEmotionJoy.isSelected = false
+                ivEmotionShocked.isSelected = false
+                ivEmotionLove.isSelected = true
+                ivEmotionShy.isSelected = false
+                ivEmotionSad.isSelected = false
+                ivEmotionAngry.isSelected = false
+            }
+            ivEmotionShy.setOnClickListener {
+                ivEmotionJoy.isSelected = false
+                ivEmotionShocked.isSelected = false
+                ivEmotionLove.isSelected = false
+                ivEmotionShy.isSelected = true
+                ivEmotionSad.isSelected = false
+                ivEmotionAngry.isSelected = false
+            }
+            ivEmotionSad.setOnClickListener {
+                ivEmotionJoy.isSelected = false
+                ivEmotionShocked.isSelected = false
+                ivEmotionLove.isSelected = false
+                ivEmotionShy.isSelected = false
+                ivEmotionSad.isSelected = true
+                ivEmotionAngry.isSelected = false
+            }
+            ivEmotionAngry.setOnClickListener {
+                ivEmotionJoy.isSelected = false
+                ivEmotionShocked.isSelected = false
+                ivEmotionLove.isSelected = false
+                ivEmotionShy.isSelected = false
+                ivEmotionSad.isSelected = false
+                ivEmotionAngry.isSelected = true
+            }
+        }
+    }
+
+    private fun colorClick() {
+        with(binding) {
+            ivEmotionJoy.setOnClickListener {
+                ivEmotionJoy.isSelected = true
+                ivEmotionShocked.isSelected = false
+                ivEmotionLove.isSelected = false
+                ivEmotionShy.isSelected = false
+                ivEmotionSad.isSelected = false
+                ivEmotionAngry.isSelected = false
+            }
+            ivEmotionShocked.setOnClickListener {
+                ivEmotionJoy.isSelected = false
+                ivEmotionShocked.isSelected = true
+                ivEmotionLove.isSelected = false
+                ivEmotionShy.isSelected = false
+                ivEmotionSad.isSelected = false
+                ivEmotionAngry.isSelected = false
+            }
+            ivEmotionLove.setOnClickListener {
+                ivEmotionJoy.isSelected = false
+                ivEmotionShocked.isSelected = false
+                ivEmotionLove.isSelected = true
+                ivEmotionShy.isSelected = false
+                ivEmotionSad.isSelected = false
+                ivEmotionAngry.isSelected = false
+            }
+            ivEmotionShy.setOnClickListener {
+                ivEmotionJoy.isSelected = false
+                ivEmotionShocked.isSelected = false
+                ivEmotionLove.isSelected = false
+                ivEmotionShy.isSelected = true
+                ivEmotionSad.isSelected = false
+                ivEmotionAngry.isSelected = false
+            }
+            ivEmotionSad.setOnClickListener {
+                ivEmotionJoy.isSelected = false
+                ivEmotionShocked.isSelected = false
+                ivEmotionLove.isSelected = false
+                ivEmotionShy.isSelected = false
+                ivEmotionSad.isSelected = true
+                ivEmotionAngry.isSelected = false
+            }
+            ivEmotionAngry.setOnClickListener {
+                ivEmotionJoy.isSelected = false
+                ivEmotionShocked.isSelected = false
+                ivEmotionLove.isSelected = false
+                ivEmotionShy.isSelected = false
+                ivEmotionSad.isSelected = false
+                ivEmotionAngry.isSelected = true
+            }
+        }
+
+    }
 }
