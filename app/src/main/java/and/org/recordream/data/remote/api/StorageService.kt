367a728f9bf959bb9964a0963bc456b191aa475c
s9hn <tnghqn@naver.com>
2022-07-20 15:33:47 +0900

    [Chore] 수정 중

--- app/src/main/java/and/org/recordream/data/remote/api/StorageService.kt
+++ app/src/main/java/and/org/recordream/data/remote/api/StorageService.kt
@@ -1,6 +1,6 @@
 package and.org.recordream.data.remote.api
 
-import and.org.recordream.data.remote.response.Record
+import and.org.recordream.data.remote.response.ResponseRecords
 import and.org.recordream.data.remote.response.ResponseWrapper
 import retrofit2.Call
 import retrofit2.http.GET
@@ -9,11 +9,11 @@ import retrofit2.http.Query
 
 interface StorageService {
 
-    @GET("record/storage/list?")
+    @GET("record/storage/list")
     fun getMyRecord(
         @Query("filter") num: Int,
         @Header("userId") userId: Int
-    ): Call<ResponseWrapper<List<Record>>>
+    ): Call<ResponseWrapper<ResponseRecords>>
 
 //    @GET("record/storage/list?filter={num}")
 //    fun getMyRecord(
