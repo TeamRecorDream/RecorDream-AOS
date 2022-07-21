d6797a1fb464862b157fa154b6567cb4286de3b2
yoon <cbj0010@naver.com>
2022-07-21 23:40:47 +0900

    [Feat] 서버 붙이는 중

--- app/src/main/java/and/org/recordream/presentation/write/WriteActivity.kt
+++ app/src/main/java/and/org/recordream/presentation/write/WriteActivity.kt
@@ -3,19 +3,34 @@ package and.org.recordream.presentation.write
 import and.org.recordream.data.remote.RecordreamClient
 import and.org.recordream.data.remote.request.RequestWrite
 import and.org.recordream.databinding.ActivityWriteBinding
+import and.org.recordream.databinding.FragmentDetailBottomSheetBinding
 import and.org.recordream.presentation.detail.DetailActivity
+import and.org.recordream.presentation.detail.DetailBottomSheetFragment
+import and.org.recordream.util.RecordreamMapping
 import and.org.recordream.util.enqueueUtil
 import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class WriteActivity : AppCompatActivity() {
     private lateinit var binding: ActivityWriteBinding
+    private lateinit var detailBottomSheet:FragmentDetailBottomSheetBinding
     private val voiceFragment = VoiceRecordFragment()
+
+    private var title =""
+    private var date = ""
+    private var content = ""
+    private var emotion = 0
+    private var dream_color = 0
+    private var genre = listOf<Int>()
+    private var note = "null"
+    private var voice = "62cdb868c3032f2b7af76531"
+    private var writer = "62c9cf068094605c781a2fb9"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +42,7 @@ class WriteActivity : AppCompatActivity() {
         wirteInitNetwork()
         emotionClick()
         colorClick()
+        clickModify()
         setContentView(binding.root)
     }
 
@@ -62,6 +78,12 @@ class WriteActivity : AppCompatActivity() {
             }
         })
     }
+    private fun clickModify(){
+        detailBottomSheet.tvDetailBottomEdit.setOnClickListener {
+            binding.tvWriteModify.visibility  = View.VISIBLE
+            binding.tvWriteWrite.visibility = View.INVISIBLE
+        }
+    }
 
     private fun savebtnActive() {
         with(binding) {
@@ -74,17 +96,25 @@ class WriteActivity : AppCompatActivity() {
         }
     }
 
-    private fun putDreamDescription() {    //꿈 내용 전달
-        val intent = Intent(this, DetailActivity::class.java)
-        startActivity(intent)
+    private var pudreamemotion = putDreamDescription()
+    private var emotionmaping = RecordreamMapping()
+    private fun putDreamDescription() {    //이모지 감정 뭐 클릭 됐는지
+        with(binding){
+            ivEmotionJoy = emotionmaping.emotionMapping()
+            ivEmotionShocked
+            ivEmotionLove
+            ivEmotionShy
+            ivEmotionSad
+            ivEmotionAngry
+        }
     }
 
     private fun wirteInitNetwork() {
         val requestWrite = RequestWrite(
-            title = "오늘은 7월 20일",
+            title = binding.tvWriteEditdream.text.toString(),
             date = "2022/07/20 (수)",
-            content = "귀엽다",
-            emotion = 2,
+            content = binding.etWriteContent.text.toString(),
+            emotion = pudreamemotion,
             dream_color = 3,
             genre = listOf(1, 2, 5),
             note = "null",
