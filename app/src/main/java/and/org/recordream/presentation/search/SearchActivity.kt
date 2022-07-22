10063de46c30acf5c56b24cd863d3b804cf84376
s9hn <tnghqn@naver.com>
2022-07-22 21:08:50 +0900

    [Feat] 검색 뷰

--- app/src/main/java/and/org/recordream/presentation/search/SearchActivity.kt
+++ app/src/main/java/and/org/recordream/presentation/search/SearchActivity.kt
@@ -1,18 +1,42 @@
 package and.org.recordream.presentation.search
 
+import and.org.recordream.data.remote.RecordreamClient
 import and.org.recordream.databinding.ActivitySearchBinding
+import and.org.recordream.util.enqueueUtil
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class SearchActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySearchBinding
+    private lateinit var searchAdapter: SearchAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySearchBinding.inflate(layoutInflater)
 
 
-
+        binding.ivSearchSearchbtn.setOnClickListener {
+            val keyword = binding.etSearchEnter.text.toString()
+            initNetwork(keyword)
+        }
         setContentView(binding.root)
+    }
+
+    private fun initNetwork(search: String) {
+        val keyword: String = search
+
+        val call = RecordreamClient.searchService.getMyRecord(keyword)
+
+        call.enqueueUtil(
+            onSuccess = {
+                Log.d("******Status******", "${it.status}")
+            }
+        )
+    }
+
+    private fun setRecyclerView() {
+
     }
 
 
