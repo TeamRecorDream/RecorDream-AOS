b0b7810dc71e5ea9cd8b9eb7cf4c018aae186da2
s9hn <tnghqn@naver.com>
2022-07-22 21:02:38 +0900

    [Feat] 검색 뷰

--- app/src/main/java/and/org/recordream/presentation/search/SearchActivity.kt
+++ app/src/main/java/and/org/recordream/presentation/search/SearchActivity.kt
@@ -6,26 +6,13 @@ import androidx.appcompat.app.AppCompatActivity
 
 class SearchActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySearchBinding
-    private lateinit var searchAdapter: SearchAdapter
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySearchBinding.inflate(layoutInflater)
 
 
-        binding.ivSearchSearchbtn.setOnClickListener {
-            initNetwork()
-        }
+
         setContentView(binding.root)
-    }
-
-    private fun initNetwork() {
-
-
-    }
-
-    private fun setRecyclerView() {
-
     }
 
 
