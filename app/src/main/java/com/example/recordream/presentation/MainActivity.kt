3d2221aa82fc476ee867692ea67adc97732a5956
yoon <cbj0010@naver.com>
2022-09-26 19:47:46 +0900

    초기세팅 activity수정

--- app/src/main/java/com/example/recordream/presentation/MainActivity.kt
+++ app/src/main/java/com/example/recordream/presentation/MainActivity.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.recordream.R
 
-class HomeActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.activity_main)
     }
 }
