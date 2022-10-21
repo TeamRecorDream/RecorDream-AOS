566a8fe525a777c2065b1bc257f9d5320e1b53b9
yoon <cbj0010@naver.com>
2022-10-22 03:18:08 +0900

    [Feat] mypage 생성

---
+++ app/src/main/java/com/recodream_aos/recordream/MypageActivity.kt
@@ -0,0 +1,11 @@
+package com.recodream_aos.recordream
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MypageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mypage)
+    }
+}
