79dab71b2bcd4a7d3da3788c88ccc665ec57bb3f
jimyu <aliyah_7@naver.com>
2022-11-03 00:33:55 +0900

    [Feat] Resonse 파일 미리 넣어놓음

--- app/src/main/java/com/recodream_aos/recordream/data/remote/response/ResponseHome.kt
+++ app/src/main/java/com/recodream_aos/recordream/data/remote/response/ResponseHome.kt
@@ -1,5 +1,11 @@
 package com.recodream_aos.recordream.data.remote.response
 
-class ResponseHome {
-    //
-}
+class ResponseHome(
+
+    val _id: String,
+    val date: String,
+    val dream_color: Int,
+    val emotion: Int,
+    val genre: List<Int>,
+    val title: String
+)
