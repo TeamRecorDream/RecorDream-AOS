8e8b727dca9947802ffed18802df99091705ad5d
s9hn <tnghqn@naver.com>
2022-12-05 04:40:45 +0900

    [FEAT(LoginViewModel)] 코드 개선
    1. 유저토큰 함수 분리

--- app/src/main/java/com/recodream_aos/recordream/presentation/login/LoginViewModel.kt
+++ app/src/main/java/com/recodream_aos/recordream/presentation/login/LoginViewModel.kt
@@ -1,8 +1,35 @@
 package com.recodream_aos.recordream.presentation.login
 
 import androidx.lifecycle.ViewModel
+import com.kakao.sdk.user.UserApiClient
+import com.kakao.sdk.user.model.AccessTokenInfo
+import timber.log.Timber
 
-class LoginViewModel : ViewModel(){
+class LoginViewModel : ViewModel() {
 
+    fun checkUserToken(): Boolean {
+        var validUserToken = false
+        UserApiClient.instance.accessTokenInfo { tokenInfo, error ->
+            if (checkErrorBoolean(error) and checkTokenInfoBoolean(tokenInfo)) {
+                validUserToken = true
+            }
+        }
+        return validUserToken
+    }
 
+    private fun checkErrorBoolean(error: Throwable?): Boolean {
+        if (error != null) {
+            Timber.tag("토큰 정보 보기 실패").d(error)
+            return false
+        }
+        return true
+    }
+
+    private fun checkTokenInfoBoolean(tokenInfo: AccessTokenInfo?): Boolean {
+        if (tokenInfo != null) {
+            Timber.d("토큰 정보 보기 성공")
+            return true
+        }
+        return false
+    }
 }
