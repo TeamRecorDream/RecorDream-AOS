e3378815211d246a60a0622658204efc5bb7b6be
s9hn <tnghqn@naver.com>
2022-12-08 15:51:03 +0900

    [FEAT(RecordreamApp)] 카카오SDK 초기화함수 추가

--- app/src/main/java/com/recodream_aos/recordream/application/RecordreamApp.kt
+++ app/src/main/java/com/recodream_aos/recordream/application/RecordreamApp.kt
@@ -1,19 +1,29 @@
-package com.recodream_aos.recordream.application
+package com.recodream_aos.recordream.application // ktlint-disable package-name
 
 import android.app.Application
-import com.recodream_aos.recordream.BuildConfig
+import com.kakao.sdk.common.KakaoSdk
+import com.recodream_aos.recordream.R
 import com.recodream_aos.recordream.util.TimberDebugTree
+import io.grpc.android.BuildConfig
 import timber.log.Timber
 
 class RecordreamApp : Application() {
     override fun onCreate() {
         super.onCreate()
         initTimber()
+        initKakaoSdk()
     }
 
     private fun initTimber() {
         if (BuildConfig.DEBUG) {
             Timber.plant(TimberDebugTree())
         }
+    }
+
+    private fun initKakaoSdk() {
+        KakaoSdk.init(
+            this,
+            this.getString(R.string.appKey_init_kakao)
+        )
     }
 }
