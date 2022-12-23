c996b2d840a7fceb9cdd150b1a065d8317c2ca8a
yoon <cbj0010@naver.com>
2022-12-24 05:30:45 +0900

    [inflate] diffutil, listadapter diffutil 생성 및 Listadapter 연결 및 변경

--- app/src/main/java/com/recodream_aos/recordream/presentation/storagy/StorageListAdapter.kt
+++ app/src/main/java/com/recodream_aos/recordream/presentation/storagy/StorageListAdapter.kt
@@ -2,37 +2,44 @@ package com.recodream_aos.recordream.presentation.storagy
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.recodream_aos.recordream.data.remote.response.ResponseStoreCard
 import com.recodream_aos.recordream.databinding.ItemListStoreListBinding
+import com.recodream_aos.recordream.util.DiffUtilItemCallback
 
-//private val itemClick: ((ResponseStoreCard) -> (Unit))
-class StorageListAdapter() :
-    RecyclerView.Adapter<StorageListAdapter.StorageViewHolder>() {
-    private var listCard = emptyList<ResponseStoreCard>()
+class StorageListAdapter(private val itemClick: (ResponseStoreCard) -> (Unit)) :
+    ListAdapter<ResponseStoreCard, StorageListAdapter.StorageViewHolder>(diffUtil) {
+//    private var listCard = emptyList<ResponseStoreCard>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StorageViewHolder {
         val binding =
             ItemListStoreListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return StorageViewHolder(binding)
+        return StorageViewHolder(binding, itemClick)
     }
 
     override fun onBindViewHolder(holder: StorageViewHolder, position: Int) {
-        holder.onBind(listCard[position])
+        holder.onBind(currentList[position])
     }
 
-    override fun getItemCount(): Int = listCard.size
-
-    fun setRepoList(repoList: List<ResponseStoreCard>) {
-        this.listCard = repoList.toList()
-        notifyDataSetChanged()
-    }
-
-    class StorageViewHolder(private val binding: ItemListStoreListBinding) :
+    class StorageViewHolder(
+        private val binding: ItemListStoreListBinding,
+        private val itemClick: (ResponseStoreCard) -> (Unit)
+    ) :
         RecyclerView.ViewHolder(binding.root) {
         fun onBind(data: ResponseStoreCard) {
             binding.card = data
+            binding.root.setOnClickListener {
+                itemClick(data)
+            }
         }
+    }
+
+    companion object {
+        val diffUtil = DiffUtilItemCallback<ResponseStoreCard>(
+            onItemsTheSame = { old, new -> old.id == new.id },
+            onContentsTheSame = { old, new -> old == new }
+        )
     }
 }
 
