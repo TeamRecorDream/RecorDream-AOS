6e0a7249c2214675f31032911fdb64e6fb2b0b12
yoon <cbj0010@naver.com>
2022-12-24 05:31:48 +0900

    [inflate] fragmentContainer연결

--- app/src/main/java/com/recodream_aos/recordream/presentation/storagy/StoragyActivity.kt
+++ app/src/main/java/com/recodream_aos/recordream/presentation/storagy/StoragyActivity.kt
@@ -1,12 +1,52 @@
 package com.recodream_aos.recordream.presentation.storagy
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.recodream_aos.recordream.R
+import com.recodream_aos.recordream.databinding.ActivityStoragyBinding
+import com.recodream_aos.recordream.presentation.storagy.fragment.StoragyGridFragment
+import com.recodream_aos.recordream.presentation.storagy.fragment.StoragyListFragment
 
 class StoragyActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityStoragyBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_storagy)
+        binding = ActivityStoragyBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        displayFragment()
+
     }
+
+    private fun displayFragment() {
+        val listFragment = StoragyListFragment()
+        val galleryFragment = StoragyGridFragment()
+        binding.ivStoreGrid.isSelected = true
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fg_store_container, StoragyGridFragment())
+            .commit()
+
+        binding.ivStoreGrid.setOnClickListener {
+            Log.d("storage", "displayFragment: grid")
+            binding.ivStoreGrid.isSelected = true
+            binding.ivStoreList.isSelected = false
+
+            changeFragment(galleryFragment)
+        }
+        binding.ivStoreList.setOnClickListener {
+            Log.d("storage", "displayFragment: list")
+            binding.ivStoreList.isSelected = true
+            binding.ivStoreGrid.isSelected = false
+            changeFragment(listFragment)
+        }
+    }
+
+    private fun changeFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fg_store_container, fragment)
+            .commit()
+    }
+
 }
