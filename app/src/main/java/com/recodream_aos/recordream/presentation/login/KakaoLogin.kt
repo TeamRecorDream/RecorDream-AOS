538ad5e2633c1aeaf3d76f32c85d4c3c68f3de8d
s9hn <tnghqn@naver.com>
2022-12-25 23:31:57 +0900

    [CHORE] MERRY CHRISTMAS - !

--- app/src/main/java/com/recodream_aos/recordream/presentation/login/KakaoLogin.kt
+++
@@ -1,7 +0,0 @@
-package com.recodream_aos.recordream.presentation.login
-
-import android.content.Context
-import dagger.hilt.android.qualifiers.ActivityContext
-import javax.inject.Inject
-
-class KakaoLogin @Inject constructor(@ActivityContext context: Context)
