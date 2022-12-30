164e5b82ae0854e7abd1b1fcc3504c24fb2edfb4
urjimyu <92876819+urjimyu@users.noreply.github.com>
2022-12-31 02:16:18 +0900

    [Feat] home refresh

--- app/src/main/java/com/recodream_aos/recordream/presentation/MainActivity.kt
+++ app/src/main/java/com/recodream_aos/recordream/presentation/MainActivity.kt
@@ -9,10 +9,15 @@ import com.recodream_aos.recordream.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.lySwipeRefresh.setOnRefreshListener {
+            binding.lySwipeRefresh.isRefreshing = false
+            //job.cancel()
+        }
 //        clickEvent()
 //        initNav()
         setOnClick()
