477b91ef8fd8c7238dcf799e9f5a13bf0cf45b0d
s9hn <tnghqn@naver.com>
2023-01-04 14:40:31 +0900

    [FEAT(LoginViewModel)] 카카오 토큰 발급 코드 구현 및 카카오토큰 서버통신 구현

--- app/src/main/java/com/recodream_aos/recordream/presentation/login/LoginViewModel.kt
+++ app/src/main/java/com/recodream_aos/recordream/presentation/login/LoginViewModel.kt
@@ -1,3 +1,47 @@
-package com.recodream_aos.recordream.presentation.login
+package com.recodream_aos.recordream.presentation.login // ktlint-disable package-name
 
-class LoginViewModel
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import com.kakao.sdk.auth.model.OAuthToken
+import com.recodream_aos.recordream.data.remote.ServciePool
+import com.recodream_aos.recordream.data.remote.api.LoginService
+
+class LoginViewModel() : ViewModel() {
+    private val loginService: LoginService = ServciePool.loginService
+    private val kakaoLoginCallback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+        if (token != null) {
+            Log.d("kakaoLogin", "카카오계정 로그인 성공")
+            Log.d("qwdqwdqwdqwdqwd", token.accessToken)
+            //  getNewToken(token.accessToken)
+        } else if (error != null) {
+            Log.d("kakaoLogin", error.toString())
+        }
+    }
+
+    //
+    fun getKaKaoCallback(): (OAuthToken?, Throwable?) -> Unit {
+        return kakaoLoginCallback
+    }
+//
+//    private fun getNewToken(kakaoToken: String) {
+//        Log.d("kakaoToken", kakaoToken)
+//        viewModelScope.launch {
+//            runCatching {
+//                loginService.postLogin(
+//                    RequestLogin(
+//                        kakaoToken,
+//                        "temp"
+//                    )
+//                )
+//            }.onSuccess {
+//                Log.d("eeeeeeeeeeeeeeeeeeeee", "${it.status}")
+//            }.onFailure {
+//                Log.d("eeeeeeeeeeeeeeeeeeeee", "$it")
+//            }
+//        }
+//    }
+}
+
+// 인터셉트 진행
+// 토큰관리
+// 자동로그인하면 완료 !
