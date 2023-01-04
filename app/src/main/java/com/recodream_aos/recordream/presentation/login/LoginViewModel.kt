f65f0b19a62844bde9f4510b8a928cddedb51379
s9hn <tnghqn@naver.com>
2023-01-04 18:29:47 +0900

    [FEAT(LoginViewModel)] 서버 결과값을 담은 stateFlow 컨테이너 추가

--- app/src/main/java/com/recodream_aos/recordream/presentation/login/LoginViewModel.kt
+++ app/src/main/java/com/recodream_aos/recordream/presentation/login/LoginViewModel.kt
@@ -2,44 +2,56 @@ package com.recodream_aos.recordream.presentation.login // ktlint-disable packag
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.kakao.sdk.auth.model.OAuthToken
 import com.recodream_aos.recordream.data.remote.ServciePool
 import com.recodream_aos.recordream.data.remote.api.LoginService
+import com.recodream_aos.recordream.data.remote.request.RequestLogin
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class LoginViewModel() : ViewModel() {
+    private val _signUpSuccess = MutableStateFlow(false)
+    val signUpSuccess: StateFlow<Boolean> get() = _signUpSuccess
     private val loginService: LoginService = ServciePool.loginService
     private val kakaoLoginCallback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
         if (token != null) {
-            Log.d("kakaoLogin", "카카오계정 로그인 성공")
-            Log.d("qwdqwdqwdqwdqwd", token.accessToken)
-            //  getNewToken(token.accessToken)
+            Log.d("*****KAKAOLOGIN/SUCCESS*****", "카카오계정 로그인 성공")
+            getNewToken(token.accessToken)
         } else if (error != null) {
-            Log.d("kakaoLogin", error.toString())
+            Log.d("*****KAKAOLOGIN/FAILURE*****", "$error")
         }
     }
 
-    //
     fun getKaKaoCallback(): (OAuthToken?, Throwable?) -> Unit {
         return kakaoLoginCallback
     }
-//
-//    private fun getNewToken(kakaoToken: String) {
-//        Log.d("kakaoToken", kakaoToken)
-//        viewModelScope.launch {
-//            runCatching {
-//                loginService.postLogin(
-//                    RequestLogin(
-//                        kakaoToken,
-//                        "temp"
-//                    )
-//                )
-//            }.onSuccess {
-//                Log.d("eeeeeeeeeeeeeeeeeeeee", "${it.status}")
-//            }.onFailure {
-//                Log.d("eeeeeeeeeeeeeeeeeeeee", "$it")
-//            }
-//        }
-//    }
+
+    private fun isSignUpSuccess() {
+        _signUpSuccess.value = true
+    }
+
+    private fun isSignUpFailure() {
+        _signUpSuccess.value = false
+    }
+
+    private fun getNewToken(kakaoToken: String) {
+        viewModelScope.launch {
+            runCatching {
+                loginService.postLogin(
+                    RequestLogin(
+                        kakaoToken,
+                        "temp"
+                    )
+                )
+            }.onSuccess {
+                isSignUpSuccess()
+            }.onFailure {
+                isSignUpFailure()
+            }
+        }
+    }
 }
 
 // 인터셉트 진행
