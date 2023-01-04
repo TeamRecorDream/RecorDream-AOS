382a2f3c93cf07ac6e34b37178198c8bbbdb805f
s9hn <tnghqn@naver.com>
2023-01-04 18:30:08 +0900

    [REFACTOR(SplashViewModel)] 잠시 주석처리

--- app/src/main/java/com/recodream_aos/recordream/presentation/splash/SplashViewModel.kt
+++ app/src/main/java/com/recodream_aos/recordream/presentation/splash/SplashViewModel.kt
@@ -5,18 +5,18 @@ import androidx.lifecycle.ViewModel
 import com.kakao.sdk.auth.model.OAuthToken
 
 class SplashViewModel : ViewModel() {
-    private val kakaoLoginCallback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
-        if (token != null) {
-            Log.d("KAKAO LOGIN", "카카오계정 로그인 성공")
-            Log.d("KAKAO TOKEN", token.accessToken)
-            //  getNewToken(token.accessToken)
-        } else if (error != null) {
-            Log.d("kakaoLogin", error.toString())
-        }
-    }
-
-    //
-    fun getKaKaoCallback(): (OAuthToken?, Throwable?) -> Unit {
-        return kakaoLoginCallback
-    }
+//    private val kakaoLoginCallback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+//        if (token != null) {
+//            Log.d("KAKAO LOGIN", "카카오계정 로그인 성공")
+//            Log.d("KAKAO TOKEN", token.accessToken)
+//            //  getNewToken(token.accessToken)
+//        } else if (error != null) {
+//            Log.d("kakaoLogin", error.toString())
+//        }
+//    }
+//
+//    //
+//    fun getKaKaoCallback(): (OAuthToken?, Throwable?) -> Unit {
+//        return kakaoLoginCallback
+//    }
 }
