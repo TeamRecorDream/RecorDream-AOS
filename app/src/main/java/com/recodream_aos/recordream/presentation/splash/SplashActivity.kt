96b3887920e6140993f54ee3199d6821f1cac796
s9hn <tnghqn@naver.com>
2023-01-04 16:59:28 +0900

    [FEAT(SplashViewModel, SplashActivity)] 데이터바인딩 태그 및 뷰모델 추가

--- app/src/main/java/com/recodream_aos/recordream/presentation/splash/SplashActivity.kt
+++ app/src/main/java/com/recodream_aos/recordream/presentation/splash/SplashActivity.kt
@@ -4,19 +4,29 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import androidx.appcompat.app.AppCompatActivity
+import android.util.Log
+import androidx.activity.viewModels
 import com.recodream_aos.recordream.R
+import com.recodream_aos.recordream.base.BindingActivity
+import com.recodream_aos.recordream.databinding.ActivitySplashBinding
 import com.recodream_aos.recordream.presentation.login.LoginActivity
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : BindingActivity<ActivitySplashBinding>(R.layout.activity_splash) {
+    private val splashViewModel: SplashViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
-
         // 김시하의 의뢰에 따라..
         // overridePendingTransition(R.anim.none, R.anim.horizon_exit)
 
+        // 토큰 갱신 우선구현 후 자동로그인 구현
+        initViewModel()
         handleSplash()
+    }
+
+    private fun initViewModel() {
+        binding.viewModel = splashViewModel
+        binding.lifecycleOwner = this
     }
 
     private fun handleSplash() {
---
+++ app/src/main/java/com/recodream_aos/recordream/presentation/splash/SplashViewModel.kt
@@ -0,0 +1,22 @@
+package com.recodream_aos.recordream.presentation.splash
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import com.kakao.sdk.auth.model.OAuthToken
+
+class SplashViewModel : ViewModel() {
+    private val kakaoLoginCallback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+        if (token != null) {
+            Log.d("KAKAO LOGIN", "카카오계정 로그인 성공")
+            Log.d("KAKAO TOKEN", token.accessToken)
+            //  getNewToken(token.accessToken)
+        } else if (error != null) {
+            Log.d("kakaoLogin", error.toString())
+        }
+    }
+
+    //
+    fun getKaKaoCallback(): (OAuthToken?, Throwable?) -> Unit {
+        return kakaoLoginCallback
+    }
+}
