be399e0340239d483d32397969cb3ca7868d7f1a
s9hn <tnghqn@naver.com>
2023-01-04 18:29:15 +0900

    [FEAT(LoginActivity)] 함수 분리 및 stateFlow를 통해 메인뷰로 넘어가는 함수 구현

--- app/src/main/java/com/recodream_aos/recordream/presentation/login/LoginActivity.kt
+++ app/src/main/java/com/recodream_aos/recordream/presentation/login/LoginActivity.kt
@@ -1,15 +1,19 @@
 package com.recodream_aos.recordream.presentation.login // ktlint-disable package-name
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.util.Utility
 import com.kakao.sdk.user.UserApiClient
 import com.recodream_aos.recordream.R
 import com.recodream_aos.recordream.base.BindingActivity
 import com.recodream_aos.recordream.databinding.ActivityLoginBinding
+import com.recodream_aos.recordream.presentation.MainActivity
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 import timber.log.Timber
 
 @AndroidEntryPoint
@@ -21,6 +25,10 @@ class LoginActivity : BindingActivity<ActivityLoginBinding>(R.layout.activity_lo
         Timber.tag("*****HASHKEY*****").d(Utility.getKeyHash(this))
         initViewModel()
         clickLoginBtn()
+//        binding.ivLoginLogo.setOnClickListener {
+//            kakaoUnlink()
+//            // 토큰재확인용
+//        }
     }
 
     private fun initViewModel() {
@@ -31,37 +39,57 @@ class LoginActivity : BindingActivity<ActivityLoginBinding>(R.layout.activity_lo
     private fun clickLoginBtn() {
         binding.clLoginKakaobtn.setOnClickListener {
             initKaKaoLogin()
+            collectSignUpResult()
         }
     }
 
     private fun initKaKaoLogin() = showKaKaoLogin(loginViewModel.getKaKaoCallback())
 
     private fun showKaKaoLogin(setCallback: (OAuthToken?, Throwable?) -> Unit) {
-        val isKaKaoLoginAvailable = UserApiClient.instance.isKakaoTalkLoginAvailable(this)
-        if (isKaKaoLoginAvailable) {
-            Log.d("kakaoLogin", "카카오톡으로 로그인 가능")
-            UserApiClient.instance.loginWithKakaoTalk(
-                this,
-                callback = setCallback
-            )
-        } else {
-            Log.d("kakaoLogin", "카카오톡으로 로그인 불가능")
-            UserApiClient.instance.loginWithKakaoAccount(
-                this,
-                callback = setCallback
-            )
+        when (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
+            true -> kakaoLoginAvailable(setCallback)
+            false -> kakaoLoginUnavailable(setCallback)
         }
     }
 
-//    private fun kakaoUnlink() {
-//        // 연결 끊기
-//        UserApiClient.instance.unlink { error ->
-//            if (error != null) {
-//                Log.d("this", "연결 끊기 실패: $error")
-//            } else {
-//                Log.d("this", "연결 끊기 성공. SDK에서 토큰 삭제 됨")
-//                // setLogin(false)
-//            }
-//        }
-//    }
+    private fun kakaoLoginAvailable(setCallback: (OAuthToken?, Throwable?) -> Unit) {
+        Log.d("*****KAKAOLOGIN/OK*****", "카카오톡으로 로그인 가능")
+        UserApiClient.instance.loginWithKakaoTalk(
+            this,
+            callback = setCallback
+        )
+    }
+
+    private fun kakaoLoginUnavailable(setCallback: (OAuthToken?, Throwable?) -> Unit) {
+        Log.d("*****KAKAOLOGIN/NO*****", "카카오톡으로 로그인 불가능")
+        UserApiClient.instance.loginWithKakaoAccount(
+            this,
+            callback = setCallback
+        )
+    }
+
+    private fun collectSignUpResult() {
+        lifecycleScope.launchWhenStarted {
+            loginViewModel.signUpSuccess.collectLatest {
+                if (it) startMainActivity()
+            }
+        }
+    }
+
+    private fun startMainActivity() {
+        val intentToMain = Intent(this, MainActivity::class.java)
+        startActivity(intentToMain)
+        finish()
+    }
+
+    private fun kakaoUnlink() {
+        // 연결 끊기
+        UserApiClient.instance.unlink { error ->
+            if (error != null) {
+                Log.d("this", "연결 끊기 실패: $error")
+            } else {
+                Log.d("this", "연결 끊기 성공. SDK에서 토큰 삭제 됨")
+            }
+        }
+    }
 }
