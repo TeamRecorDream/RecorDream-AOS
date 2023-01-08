f09b181705ebdafe0666ac75928f068808a03c69
s9hn <tnghqn@naver.com>
2023-01-08 23:57:33 +0900

    [FEAT(AuthDataSource)] 새 토큰 발급메소드 추가

--- app/src/main/java/com/recodream_aos/recordream/data/datasource/remote/AuthDataSource.kt
+++ app/src/main/java/com/recodream_aos/recordream/data/datasource/remote/AuthDataSource.kt
@@ -1,10 +1,16 @@
 package com.recodream_aos.recordream.data.datasource.remote // ktlint-disable package-name
 
 import com.recodream_aos.recordream.data.entity.remote.response.ResponseLogin
+import com.recodream_aos.recordream.data.entity.remote.response.ResponseNewToken
 
 interface AuthDataSource {
     suspend fun postLogin(
         kakaoToken: String,
         fcmToken: String
     ): ResponseLogin
+
+    suspend fun getNewToken(
+        accessToken: String,
+        refreshToken: String
+    ): ResponseNewToken
 }
