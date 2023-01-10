2b88f9515aa9dbdbf222a3b9039d3d8bbedfa20b
s9hn <tnghqn@naver.com>
2023-01-10 18:41:35 +0900

    [FEAT(SplashViewModel)] 로컬 토큰 유무를 파악하는 로직설계
    1. 로컬에 저장된 토큰을 리퀘스트로 보낸다.
    2. 400대가 나올 경우 토큰이 없음 -> LoginActivity
    3. 200이 나올 경우 -> MainActivity

--- app/src/main/java/com/recodream_aos/recordream/presentation/splash/SplashViewModel.kt
+++ app/src/main/java/com/recodream_aos/recordream/presentation/splash/SplashViewModel.kt
@@ -1,22 +1,28 @@
-package com.recodream_aos.recordream.presentation.splash
+package com.recodream_aos.recordream.presentation.splash // ktlint-disable package-name
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
-import com.kakao.sdk.auth.model.OAuthToken
+import androidx.lifecycle.viewModelScope
+import com.recodream_aos.recordream.domain.repository.AuthRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class SplashViewModel : ViewModel() {
-//    private val kakaoLoginCallback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
-//        if (token != null) {
-//            Log.d("KAKAO LOGIN", "카카오계정 로그인 성공")
-//            Log.d("KAKAO TOKEN", token.accessToken)
-//            //  getNewToken(token.accessToken)
-//        } else if (error != null) {
-//            Log.d("kakaoLogin", error.toString())
-//        }
-//    }
-//
-//    //
-//    fun getKaKaoCallback(): (OAuthToken?, Throwable?) -> Unit {
-//        return kakaoLoginCallback
-//    }
+@HiltViewModel
+class SplashViewModel @Inject constructor(
+    private val authRepository: AuthRepository
+) : ViewModel() {
+    private val _isLoginSuccess = MutableStateFlow(false)
+    val isLoginSuccess: StateFlow<Boolean> get() = _isLoginSuccess
+
+    private fun isLoginSuccess() {
+        _isLoginSuccess.value = true
+    }
+
+    fun tryLogin() {
+        viewModelScope.launch {
+            if (authRepository.tryLogin()) isLoginSuccess()
+        }
+    }
 }
