58ba5319ba6fc9a4bf581c11095955d0d6f1200a
s9hn <tnghqn@naver.com>
2023-01-10 18:45:46 +0900

    [FEAT(SplashActivity)] 스플래시뷰모델 구독을 통해 엑티비티 이동 분기처리

--- app/src/main/java/com/recodream_aos/recordream/presentation/splash/SplashActivity.kt
+++ app/src/main/java/com/recodream_aos/recordream/presentation/splash/SplashActivity.kt
@@ -1,25 +1,30 @@
 package com.recodream_aos.recordream.presentation.splash // ktlint-disable package-name
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.util.Log
 import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.recodream_aos.recordream.R
 import com.recodream_aos.recordream.base.BindingActivity
 import com.recodream_aos.recordream.databinding.ActivitySplashBinding
+import com.recodream_aos.recordream.presentation.MainActivity
 import com.recodream_aos.recordream.presentation.login.LoginActivity
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 
+@AndroidEntryPoint
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : BindingActivity<ActivitySplashBinding>(R.layout.activity_splash) {
     private val splashViewModel: SplashViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // 김시하의 의뢰에 따라..
+        // 뷰 등장 애니메이션 설정, TAG : 시하
         // overridePendingTransition(R.anim.none, R.anim.horizon_exit)
 
-        // 토큰 갱신 우선구현 후 자동로그인 구현
         initViewModel()
         handleSplash()
     }
@@ -31,16 +36,32 @@ class SplashActivity : BindingActivity<ActivitySplashBinding>(R.layout.activity_
 
     private fun handleSplash() {
         val splashHandler = Handler(Looper.getMainLooper())
-        val intentToLogin = Intent(this, LoginActivity::class.java)
         splashHandler.postDelayed(
             {
-                intentToLogin.apply {
-                    startActivity(intentToLogin)
-                    finish()
-                }
+                isLoginAble()
+                finish()
             },
             NINE_HUNDRED
         )
+    }
+
+    private fun isLoginAble() {
+        splashViewModel.tryLogin()
+        lifecycleScope.launchWhenStarted {
+            splashViewModel.isLoginSuccess.collectLatest {
+                if (it) startMainActivity() else startLoginActivity()
+            }
+        }
+    }
+
+    private fun startMainActivity() {
+        val intentToMain = Intent(this, MainActivity::class.java)
+        startActivity(intentToMain)
+    }
+
+    private fun startLoginActivity() {
+        val intentToLogin = Intent(this, LoginActivity::class.java)
+        startActivity(intentToLogin)
     }
 
     companion object {
