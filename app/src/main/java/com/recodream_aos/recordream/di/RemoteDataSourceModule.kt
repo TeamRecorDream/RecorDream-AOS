29821208c1ec49c2ef30d0952e65e972af68aa8f
s9hn <tnghqn@naver.com>
2023-01-10 16:47:40 +0900

    [REFACTOR(RemoteDataSourceModule)] 서비스이름 변경

--- app/src/main/java/com/recodream_aos/recordream/di/RemoteDataSourceModule.kt
+++ app/src/main/java/com/recodream_aos/recordream/di/RemoteDataSourceModule.kt
@@ -1,6 +1,6 @@
 package com.recodream_aos.recordream.di // ktlint-disable package-name
 
-import com.recodream_aos.recordream.data.api.LoginService
+import com.recodream_aos.recordream.data.api.AuthService
 import com.recodream_aos.recordream.data.datasource.remote.AuthDataSource
 import com.recodream_aos.recordream.data.datasource.remote.AuthDataSourceImpl
 import dagger.Module
@@ -16,6 +16,6 @@ object RemoteDataSourceModule {
     @Provides
     @Singleton
     fun providesAuthDataSourceImpl(
-        loginService: LoginService
-    ): AuthDataSource = AuthDataSourceImpl(loginService)
+        authService: AuthService
+    ): AuthDataSource = AuthDataSourceImpl(authService)
 }
