919a215fb95ddb2498e2776377f9a1c3e85e79df
s9hn <tnghqn@naver.com>
2023-01-14 03:09:31 +0900

    [REFACTOR(AuthDataSourceImpl)] CODE ROLLBACK

--- app/src/main/java/com/recodream_aos/recordream/data/datasource/remote/AuthDataSourceImpl.kt
+++ app/src/main/java/com/recodream_aos/recordream/data/datasource/remote/AuthDataSourceImpl.kt
@@ -1,6 +1,5 @@
 package com.recodream_aos.recordream.data.datasource.remote // ktlint-disable package-name
 
-import android.util.Log
 import com.recodream_aos.recordream.data.api.AuthService
 import com.recodream_aos.recordream.data.entity.remote.request.RequestLogin
 import com.recodream_aos.recordream.data.entity.remote.response.ResponseLogin
@@ -20,9 +19,6 @@ class AuthDataSourceImpl @Inject constructor(
         accessToken: String,
         refreshToken: String
     ): ResponseWrapper<ResponseNewToken> {
-        val token = authService.postToken(accessToken, "refrweshToken")
-        Log.d("안녕", "${token.message}AuthDataSource")
-        Log.d("안녕", token.success.toString())
-        return token
+        return authService.postToken(accessToken, refreshToken)
     }
 }
