d9baa4fffc69bc52041bd0fc419afb87ba83331b
s9hn <tnghqn@naver.com>
2023-01-14 03:43:16 +0900

    [FEAT(SplashViewModel)] Code Refactoring
    1. init tryLogin by SplashViewModel
    2. StateFlow value with empty is suitable to sharedFlow -> sealed class
    3. Create sealed class with object of 3

--- app/src/main/java/com/recodream_aos/recordream/presentation/splash/SplashViewModel.kt
+++ app/src/main/java/com/recodream_aos/recordream/presentation/splash/SplashViewModel.kt
@@ -1,6 +1,5 @@
 package com.recodream_aos.recordream.presentation.splash // ktlint-disable package-name
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.recodream_aos.recordream.domain.repository.AuthRepository
@@ -8,18 +7,15 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
 class SplashViewModel @Inject constructor(
     private val authRepository: AuthRepository
 ) : ViewModel() {
-    private val _isLoginSuccess = MutableStateFlow(false)
-    val isLoginSuccess: StateFlow<Boolean> get() = _isLoginSuccess
-
-    private fun isLoginSuccess() {
-        _isLoginSuccess.value = true
-    }
+    private val _isLoginSuccess = MutableStateFlow<LoginState>(LoginState.IDLE)
+    val isLoginSuccess: StateFlow<LoginState> get() = _isLoginSuccess
 
     init {
         tryLogin()
@@ -27,8 +23,22 @@ class SplashViewModel @Inject constructor(
 
     private fun tryLogin() {
         viewModelScope.launch {
-            Log.d("안녕", "${authRepository.postToken()}뷰모델입ㅂ니다 현재")
-            if (authRepository.postToken()) isLoginSuccess()
+            if (authRepository.postToken()) isLoginSuccess() else isLoginFailure()
+            Timber.e("SplashViewModel2 : ${authRepository.postToken()}")
         }
+    }
+
+    private fun isLoginSuccess() {
+        _isLoginSuccess.value = LoginState.SUCCESS
+    }
+
+    private fun isLoginFailure() {
+        _isLoginSuccess.value = LoginState.FAIL
+    }
+
+    sealed class LoginState {
+        object SUCCESS : LoginState()
+        object FAIL : LoginState()
+        object IDLE : LoginState()
     }
 }
