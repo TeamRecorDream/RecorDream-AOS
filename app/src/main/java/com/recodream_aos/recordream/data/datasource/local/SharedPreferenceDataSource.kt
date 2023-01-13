d4439fa9922a81c64fb1ec14659ab97d614df8e9
s9hn <tnghqn@naver.com>
2023-01-14 00:53:42 +0900

    [REFACTOR(SharedPreferenceDataSource)] methods can be nullable

--- app/src/main/java/com/recodream_aos/recordream/data/datasource/local/SharedPreferenceDataSource.kt
+++ app/src/main/java/com/recodream_aos/recordream/data/datasource/local/SharedPreferenceDataSource.kt
@@ -2,9 +2,9 @@ package com.recodream_aos.recordream.data.datasource.local // ktlint-disable pac
 
 interface SharedPreferenceDataSource {
 
-    fun setAccessToken(accessToken: String)
+    fun setAccessToken(accessToken: String?)
 
-    fun setRefreshToken(refreshToken: String)
+    fun setRefreshToken(refreshToken: String?)
 
     fun getAccessToken(): String
 
