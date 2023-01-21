b2a652956c03a62978eb146209fc566c4950636c
s9hn <tnghqn@naver.com>
2023-01-21 23:08:55 +0900

    [FEAT(CalenderDialog)] Create CalenderDialog class

---
+++ app/src/main/java/com/recodream_aos/recordream/util/CalenderDialog.kt
@@ -0,0 +1,26 @@
+package com.recodream_aos.recordream.util // ktlint-disable package-name
+
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.icu.util.Calendar
+import android.os.Bundle
+import android.widget.DatePicker
+import androidx.fragment.app.DialogFragment
+
+class CalenderDialog : DialogFragment(), DatePickerDialog.OnDateSetListener {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        // Use the current date as the default date in the picker
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        // Create a new instance of DatePickerDialog and return it
+        return DatePickerDialog(requireContext(), this, year, month, day)
+    }
+
+    override fun onDateSet(view: DatePicker, year: Int, month: Int, day: Int) {
+        // Do something with the date chosen by the user
+    }
+}
