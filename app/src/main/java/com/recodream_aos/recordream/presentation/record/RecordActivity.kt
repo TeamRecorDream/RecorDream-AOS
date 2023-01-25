9ddafe17bfc25787677797bddc339d68a048979e
s9hn <tnghqn@naver.com>
2023-01-26 04:34:22 +0900

    [FEAT(RecordActivity)] init DatePickerDialog

--- app/src/main/java/com/recodream_aos/recordream/presentation/record/RecordActivity.kt
+++ app/src/main/java/com/recodream_aos/recordream/presentation/record/RecordActivity.kt
@@ -3,32 +3,40 @@ package com.recodream_aos.recordream.presentation.record // ktlint-disable packa
 import android.app.DatePickerDialog
 import android.icu.util.Calendar
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.recodream_aos.recordream.R
 import com.recodream_aos.recordream.base.BindingActivity
 import com.recodream_aos.recordream.databinding.ActivityRecordBinding
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
 
 class RecordActivity : BindingActivity<ActivityRecordBinding>(R.layout.activity_record) {
+    private val recordViewModel: RecordViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initViewModel()
+        setClickListener()
+    }
 
-        binding.tvRecordDateNum.text =
-            LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
-
-        binding.clRecordDateBtn.setOnClickListener {
-            val cal = Calendar.getInstance()
-
-            val data = DatePickerDialog.OnDateSetListener { view, year, month, day ->
-                binding.tvRecordRecordingTime.text = "$year/$month/$day"
-            }
-            DatePickerDialog(
-                this,
-                data,
-                cal.get(Calendar.YEAR),
-                cal.get(Calendar.MONTH),
-                cal.get(Calendar.DAY_OF_MONTH)
-            ).show()
+    private fun setClickListener() {
+        with(binding) {
+            clRecordDateBtn.setOnClickListener { initDatePickerDialog() }
         }
+    }
+
+    private fun initDatePickerDialog() {
+        val cal = Calendar.getInstance()
+
+        DatePickerDialog(
+            this,
+            recordViewModel.initDate(),
+            cal.get(Calendar.YEAR),
+            cal.get(Calendar.MONTH),
+            cal.get(Calendar.DAY_OF_MONTH)
+        ).show()
+    }
+
+    private fun initViewModel() {
+        binding.viewModel = recordViewModel
+        binding.lifecycleOwner = this
     }
 }
