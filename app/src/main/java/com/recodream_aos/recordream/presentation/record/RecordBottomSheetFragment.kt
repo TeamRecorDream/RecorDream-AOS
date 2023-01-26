2f03bace5a09ed59a497492419581d472d175800
s9hn <tnghqn@naver.com>
2023-01-27 01:28:34 +0900

    [FEAT(RecordBottomSheetFragment)] Create record dialog

---
+++ app/src/main/java/com/recodream_aos/recordream/presentation/record/RecordBottomSheetFragment.kt
@@ -0,0 +1,20 @@
+package com.recodream_aos.recordream.presentation.record // ktlint-disable package-name
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.recodream_aos.recordream.R
+
+class RecordBottomSheetFragment : BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_record_bottom_sheet, container, false)
+    }
+}
