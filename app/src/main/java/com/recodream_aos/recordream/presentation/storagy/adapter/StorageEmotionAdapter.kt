3738e162c2a9559ff2259c8e8f383b6f4ebba1a5
yoon <cbj0010@naver.com>
2023-01-30 16:01:20 +0900

    [chore] 에러 고치기 Inactivity, disconnevting from the service이거 뭐냥

--- app/src/main/java/com/recodream_aos/recordream/presentation/storagy/adapter/StorageEmotionAdapter.kt
+++ app/src/main/java/com/recodream_aos/recordream/presentation/storagy/adapter/StorageEmotionAdapter.kt
@@ -1,29 +1,37 @@
 package com.recodream_aos.recordream.presentation.storagy.adapter
 
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.recodream_aos.recordream.data.entity.local.StorageEmotionData
 import com.recodream_aos.recordream.databinding.ItemListStoreMyemotionBinding
 
 class StorageEmotionAdapter : RecyclerView.Adapter<StorageEmotionAdapter.EmotionViewHolder>() {
-
-
+    val emotionList = mutableListOf<StorageEmotionData>()
+    var emotionQuery = 0
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EmotionViewHolder {
         TODO("Not yet implemented")
     }
 
     override fun onBindViewHolder(holder: EmotionViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.onBind(emotionList[position])
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = emotionList.size
 
-
-    class EmotionViewHolder(private val binding: ItemListStoreMyemotionBinding) :
+    class EmotionViewHolder(
+        private val binding: ItemListStoreMyemotionBinding,
+        private val emotionItemClick: (Int, View) -> Unit
+    ) :
         RecyclerView.ViewHolder(binding.root) {
-//        fun onBind(data: ) {
-//
-//        }
+        fun onBind(data: StorageEmotionData) {
+            binding.clStoreEmotion.setOnClickListener {
+                emotionItemClick(
+                    adapterPosition, it
+                )
+            }
+            binding.ivStoreListEmotion.setImageResource(data.feeling)
+            binding.tvStoreEmotion.text = data.feelingText
+        }
     }
 }
