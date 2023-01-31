cec879747308cda8207d833853596e73a28ec938
yoon <cbj0010@naver.com>
2023-01-31 13:47:15 +0900

    [feat] hilt 어노테이션 적용

--- app/src/main/java/com/recodream_aos/recordream/presentation/storagy/fragment/StoragyGridFragment.kt
+++ app/src/main/java/com/recodream_aos/recordream/presentation/storagy/fragment/StoragyGridFragment.kt
@@ -6,10 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.recodream_aos.recordream.R
 import com.recodream_aos.recordream.databinding.FragmentStoragyGridBinding
 import com.recodream_aos.recordream.presentation.document.DocumentActivity
+import com.recodream_aos.recordream.presentation.storagy.MyDecoration
 import com.recodream_aos.recordream.presentation.storagy.adapter.StorageGridAdapter
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class StoragyGridFragment : Fragment() {
     private var _binding: FragmentStoragyGridBinding? = null
     private val binding get() = _binding ?: error("Binding이 연결되지 않았습니다.")
@@ -30,6 +34,13 @@ class StoragyGridFragment : Fragment() {
                 val intent = Intent(requireContext(), DocumentActivity::class.java)
             }
         binding.rvStoreGrid.adapter = storageGridAdapter
+        initItemDecoration()
+    }
+
+    private fun initItemDecoration() {
+        binding.rvStoreGrid.addItemDecoration(
+            MyDecoration(resources.getDimensionPixelOffset(R.dimen.margin_15), 1)
+        )
     }
 
     override fun onDestroyView() {
