2d3836e517b6f0218adebcd50319e05aca03ae25
yoon <cbj0010@naver.com>
2023-02-02 04:17:48 +0900

    [feat] MainActivity.kt bottomNavi 수정 및 navi삭제

--- app/src/main/java/com/recodream_aos/recordream/presentation/MainActivity.kt
+++ app/src/main/java/com/recodream_aos/recordream/presentation/MainActivity.kt
@@ -1,80 +1,55 @@
 package com.recodream_aos.recordream.presentation
 
-import android.content.Intent
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.recodream_aos.recordream.R
 import com.recodream_aos.recordream.base.BindingActivity
 import com.recodream_aos.recordream.databinding.ActivityMainBinding
-import com.recodream_aos.recordream.presentation.mypage.MypageActivity
+import com.recodream_aos.recordream.presentation.home.HomeFragment
 import com.recodream_aos.recordream.presentation.storagy.fragment.StorageFragment
+import com.recodream_aos.recordream.presentation.storagy.fragment.StoragyListFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-//        clickEvent()
-//        initNav()
-        setOnClick()
+        initTransaction()
     }
 
-    private fun displayFragment() {
-//        supportFragmentManager.beginTransaction()
-//            .add(R.id.home_container, HomeFragment())
-//            .commit()
-//
-//        binding.bnvHome.setOnItemSelectedListener {
-//            changeFragment(
-//                when(it.itemId){
-//                    R.id.home_menu -> HomeFragment()
-//                    R.id.gallery_menu -> GalleryFragment()
-//                    else -> SearchFragment()
-//                }
-//            )
-//            true
-//        }
-//        binding.bnvHome.selectedItemId = R.id.home_menu
-//    }
-//
-//    private fun changeFragment(fragment: Fragment){
-//        supportFragmentManager
-//            .beginTransaction()
-//            .replace(R.id.home_container,fragment)
-//            .commit()
-    }
-
-//    private fun initNav() {
-//        NavigationUI.setupWithNavController(
-//            binding.bnvMainCustomnav,
-//            findNavController(R.id.fcv_main_navhostfragment)
-//        )
-//    }
-
-    //    private fun clickEvent() {
-//        with(binding) {
-//            ivMainMypage.setOnClickListener {
-//                val intent = Intent(this@MainActivity, MypageActivity::class.java)
-//                startActivity(intent)
-//            }
-//
-//            ivMainWritebtn.setOnClickListener {
-//                val intent = Intent(this@MainActivity, WriteActivity::class.java)
-//                startActivity(intent)
-//            }
-//            ivMainSearch.setOnClickListener {
-//                val intent = Intent(this@MainActivity, SearchActivity::class.java)
-//                startActivity(intent)
-//            }
-//        }
-    private fun setOnClick() {
-        binding.ivMainMypage.setOnClickListener {
-            val intent = Intent(this, MypageActivity::class.java)
-            startActivity(intent)
+    private fun initTransaction() {
+        supportFragmentManager.beginTransaction().add(R.id.fcv_main_navhostfragment, HomeFragment())
+            .commit()
+        binding.bnvMainCustomnav.setOnItemSelectedListener { item ->
+            changeFragment(
+                when (item.itemId) {
+                    R.id.menu_home -> HomeFragment()
+                    else -> StorageFragment()
+                }
+            )
+            true
         }
-        binding.ivMainSearch.setOnClickListener {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.fcv_main_navhostfragment, StorageFragment()).commit()
-        }
-
+        binding.bnvMainCustomnav.setOnItemReselectedListener {}
     }
+
+    private fun changeFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fcv_main_navhostfragment, fragment)
+            .commit()
+    }
+
+//    private fun setOnClick() {
+//        binding.ivMainMypage.setOnClickListener {
+//            val intent = Intent(this, MypageActivity::class.java)
+//            startActivity(intent)
+//        }
+//        binding.ivMainSearch.setOnClickListener {
+//            supportFragmentManager.beginTransaction()
+//                .replace(R.id.fcv_main_navhostfragment, StoragyListFragment()).commit()
+//        }
+//    }
+
 }
