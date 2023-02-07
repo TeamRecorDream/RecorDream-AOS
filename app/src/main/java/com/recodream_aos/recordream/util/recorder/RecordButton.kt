8248fb79e552f397b209160bca09d10e14dd3976
s9hn <tnghqn@naver.com>
2023-02-07 23:56:52 +0900

    [REFACTOR(RecordButton)] Refactor code

--- app/src/main/java/com/recodream_aos/recordream/util/recorder/RecordButton.kt
+++ app/src/main/java/com/recodream_aos/recordream/util/recorder/RecordButton.kt
@@ -10,15 +10,11 @@ class RecordButton(
     attributeSet: AttributeSet? = null
 ) : AppCompatImageButton(context, attributeSet) {
 
-    init {
-        setBackgroundResource(R.drawable.icn_mic_start)
-    }
-
     fun updateIconWithState(state: RecordButtonState) {
         when (state) {
-            RecordButtonState.BEFORE_RECORDING -> setImageResource(R.drawable.icn_mic_start)
-            RecordButtonState.ON_RECORDING -> setImageResource(R.drawable.icn_mic_stop)
-            RecordButtonState.AFTER_RECORDING -> setImageResource(R.drawable.icn_mic_reset)
+            RecordButtonState.BEFORE_RECORDING -> setBackgroundResource(R.drawable.icn_mic_start)
+            RecordButtonState.ON_RECORDING -> setBackgroundResource(R.drawable.icn_mic_stop)
+            RecordButtonState.AFTER_RECORDING -> setBackgroundResource(R.drawable.icn_mic_reset)
         }
     }
 }
