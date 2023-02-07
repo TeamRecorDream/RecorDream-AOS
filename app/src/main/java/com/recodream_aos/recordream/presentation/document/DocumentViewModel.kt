b66fad3957f3a148c8592f0fbf3540ef878b6a00
jimyu <aliyah_7@naver.com>
2023-02-07 16:39:35 +0900

    [Feature] Edited DocumentViewModel

--- app/src/main/java/com/recodream_aos/recordream/presentation/document/DocumentViewModel.kt
+++ app/src/main/java/com/recodream_aos/recordream/presentation/document/DocumentViewModel.kt
@@ -1,7 +1,15 @@
 package com.recodream_aos.recordream.presentation.document
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DocumentViewModel:ViewModel() {
-
+class DocumentViewModel : ViewModel() {
+    var icon = MutableLiveData<Int>()
+    var date = MutableLiveData<String>()
+    var title = MutableLiveData<String>()
+    var genre1 = MutableLiveData<Int>()
+    var genre2 = MutableLiveData<Int>()
+    var genre3 = MutableLiveData<Int>()
+    var diary = MutableLiveData<String>()
+    var card = MutableLiveData<Int>()
 }
