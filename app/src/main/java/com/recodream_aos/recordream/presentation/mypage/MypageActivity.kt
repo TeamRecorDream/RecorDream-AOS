55a3127d8a20cfc1c427657e6316db3fd18a1be5
yoon <cbj0010@naver.com>
2023-02-26 20:13:09 +0900

    [Chore] pull develop

--- app/src/main/java/com/recodream_aos/recordream/presentation/mypage/MypageActivity.kt
+++ app/src/main/java/com/recodream_aos/recordream/presentation/mypage/MypageActivity.kt
@@ -2,10 +2,8 @@ package com.recodream_aos.recordream.presentation.mypage
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.messaging.FirebaseMessaging
 import com.recodream_aos.recordream.R
 import com.recodream_aos.recordream.databinding.ActivityMypageBinding
 import com.recodream_aos.recordream.presentation.login.LoginActivity
@@ -21,17 +19,6 @@ class MypageActivity : AppCompatActivity() {
         binding = ActivityMypageBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setOnClick()
-        initFirebase()
-    }
-
-    private val firebaseToken: TextView by lazy { findViewById(R.id.tv_fire) }
-
-    private fun initFirebase() {
-        FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
-            if (task.isSuccessful) {
-                firebaseToken.text = task.result
-            }
-        }
     }
 
     private fun setOnClick() {
