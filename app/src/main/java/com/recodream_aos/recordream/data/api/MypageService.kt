9113d9f7a3fbe6e8545d0f5569828a6739ef02da
yoon <cbj0010@naver.com>
2023-03-10 23:55:33 +0900

    [feat[mypage]] add pushAlam post imp

--- app/src/main/java/com/recodream_aos/recordream/data/api/MypageService.kt
+++ app/src/main/java/com/recodream_aos/recordream/data/api/MypageService.kt
@@ -1,10 +1,17 @@
 package com.recodream_aos.recordream.data.api
 
+import com.recodream_aos.recordream.data.entity.remote.request.RequestPushAlam
 import com.recodream_aos.recordream.data.entity.remote.response.ResponseMypageUser
 import com.recodream_aos.recordream.data.entity.remote.response.ResponseWrapper
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface MypageService {
     @GET("user")
     suspend fun getUser(): ResponseWrapper<ResponseMypageUser>
+
+    @POST("/user/notice")
+    suspend fun postPushAlam(@Body requestTime: RequestPushAlam): ResponseWrapper<String>
+
 }
