ed71e1decb627b0cc91dcebb7f3c1730a7d45370
yoon <cbj0010@naver.com>
2023-03-15 19:45:31 +0900

    [Feat[mypage]] add RequestPushToken.kt

--- app/src/main/java/com/recodream_aos/recordream/data/entity/remote/request/RequestPushToken.kt
+++ app/src/main/java/com/recodream_aos/recordream/data/entity/remote/request/RequestPushToken.kt
@@ -3,7 +3,6 @@ package com.recodream_aos.recordream.data.entity.remote.request
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class RequestPushToken(
-    val newToken: String,
-    val originToken: String
+data class RequestFcmToken(
+    val fcmToken: String
 )
