69b9f17d5c709a29121eed8ab491e411c350bb9b
jimyu <aliyah_7@naver.com>
2023-04-16 23:51:46 +0900

    [Feat] 홈뷰 서버통신 인터페이스 생성

---
+++ app/src/main/java/com/recodream_aos/recordream/data/api/DocumentService.kt
@@ -0,0 +1,20 @@
+package com.recodream_aos.recordream.data.api
+
+import com.recodream_aos.recordream.data.entity.remote.response.ResponseDocument
+import com.recodream_aos.recordream.data.entity.remote.response.ResponseWrapper
+import retrofit2.Call
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface DocumentService {
+    @GET("record/{recordId}")
+    fun getDetailRecord(
+        @Path("recordId") recordId: String
+    ): Call<ResponseWrapper<ResponseDocument>>
+
+    @DELETE("record/{recordId}")
+    fun deleteDetailRecord(
+        @Path("recordId") recordId: String
+    ): Call<ResponseWrapper<ResponseDocument>>
+}
