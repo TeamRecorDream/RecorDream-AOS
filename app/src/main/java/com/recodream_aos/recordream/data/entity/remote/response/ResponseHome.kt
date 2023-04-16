53fe36eb46b418da1a497f498437790df681b6b8
jimyu <aliyah_7@naver.com>
2023-04-16 23:52:46 +0900

    [Feat] 홈뷰 서버통신 데이터 클래스 업데이트

--- app/src/main/java/com/recodream_aos/recordream/data/entity/remote/response/ResponseHome.kt
+++ app/src/main/java/com/recodream_aos/recordream/data/entity/remote/response/ResponseHome.kt
@@ -1,11 +1,25 @@
 package com.recodream_aos.recordream.data.entity.remote.response
 
-data class ResponseHome(
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
-    val _id: String,
-    val date: String,
-    val dream_color: Int,
-    val emotion: Int,
-    val genre: List<Int>,
-    val title: String
-)
+@Serializable
+data class ResponseHome(
+    val nickname: String,
+    @SerialName("records")
+    val records: List<Record>
+) {
+    @Serializable
+    data class Record(
+        @SerialName("_id")
+        val id: String,
+        @SerialName("date")
+        val date: String,
+        val dream_color: Int,
+        val emotion: Int,
+        val genre: List<Int>,
+        val title: String,
+        @SerialName("isExistVoice")
+        val voice: Boolean
+    )
+}
