c016b2afdcdad49924791a364cc0cf9ff77be78a
s9hn <tnghqn@naver.com>
2023-05-07 23:20:52 +0900

    [REFACTOR(activity_record)] 각 뷰 컴포넌트에 selector적용 및 텍스트,힌트 컬러색 변경

--- app/src/main/java/com/recodream_aos/recordream/presentation/MainActivity.kt
+++ app/src/main/java/com/recodream_aos/recordream/presentation/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
                 when (item.itemId) {
                     R.id.menu_home -> HomeFragment()
                     else -> StorageFragment()
-                }
+                },
             )
             true
         }
