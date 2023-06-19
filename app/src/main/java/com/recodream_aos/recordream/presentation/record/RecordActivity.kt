8c758fc57f4dfb6791889d4f3e77861e35079bf3
SeHun <81347125+s9hn@users.noreply.github.com>
2023-06-19 14:16:37 +0900

    refactor : 현재 날짜로부터 미래 날짜 선택 불가

--- app/src/main/java/com/recodream_aos/recordream/presentation/record/RecordActivity.kt
+++ app/src/main/java/com/recodream_aos/recordream/presentation/record/RecordActivity.kt
@@ -61,7 +61,10 @@ class RecordActivity : BindingActivity<ActivityRecordBinding>(R.layout.activity_
             cal.get(Calendar.YEAR),
             cal.get(Calendar.MONTH),
             cal.get(Calendar.DAY_OF_MONTH),
-        ).show()
+        ).apply {
+            datePicker.maxDate = System.currentTimeMillis()
+            show()
+        }
     }
 
     private fun initRecordBottomSheetDialog() {
