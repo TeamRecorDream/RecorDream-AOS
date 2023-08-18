53964242d68cc7c38d7e09dfe72e57633a0dc023
yoon <cbj0010@naver.com>
2023-08-18 14:58:59 +0900

    [chore] itemdecoration수정

--- app/src/main/java/com/team/recordream/presentation/storagy/MyDecoration.kt
+++ app/src/main/java/com/team/recordream/presentation/storagy/MyDecoration.kt
@@ -16,7 +16,7 @@ class MyDecoration(private val divHeight: Int, private val rowcount: Int) :
     ) {
         super.getItemOffsets(outRect, view, parent, state)
 
-        val postion = parent.getChildAdapterPosition(view) // 어댑터에서 지정된 view에 해당하는 위치를 반환하는 메서드다.
+        val position = parent.getChildAdapterPosition(view) // 어댑터에서 지정된 view에 해당하는 위치를 반환하는 메서드다.
 
         // followerRecyclerview
         if (rowcount == 1) { // 행이 1개일때
@@ -28,13 +28,24 @@ class MyDecoration(private val divHeight: Int, private val rowcount: Int) :
             ) // left, top, right, bottom
         } else { // repositoryrecyclerview
             outRect.top = divHeight
-            outRect.bottom = divHeight / 2
-            if (postion % rowcount == 0) {
+            outRect.bottom = divHeight / divHeight
+            if (position % rowcount == 0) {
                 outRect.left = divHeight
                 outRect.right = divHeight / 2
             } else {
                 outRect.left = divHeight / 2
                 outRect.right = divHeight
+            }
+        }
+        if (position >= 2) {
+            val column = position % rowcount // item column
+            outRect.apply {
+                // divHeight - column * ((1f / rowcount) * divHeight)
+                left = divHeight - column * divHeight / rowcount
+                // (column + 1) * ((1f / rowcount) * divHeight)
+                right = (column + 1) * divHeight / rowcount
+//                if (position < rowcount) top = divHeight
+//                bottom = divHeight
             }
         }
     }
