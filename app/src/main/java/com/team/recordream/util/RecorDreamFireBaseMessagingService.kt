5a6d8e99fb454e1a115e3dc2aa14daf318633d7a
yoon <cbj0010@naver.com>
2023-08-30 22:08:59 +0900

    [chore[pushAlarm]] 푸시알림 로고 적용

--- app/src/main/java/com/team/recordream/util/RecorDreamFireBaseMessagingService.kt
+++ app/src/main/java/com/team/recordream/util/RecorDreamFireBaseMessagingService.kt
@@ -6,6 +6,7 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.util.Log
 import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.messaging.FirebaseMessagingService
@@ -40,7 +41,10 @@ class RecorDreamFireBaseMessagingService : FirebaseMessagingService() {
             PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
         val notificationBuilder = Notification.Builder(this, CHANNEL_ID)
             .setContentTitle(title)
-            .setSmallIcon(R.mipmap.ic_launcher)
+            .setColor(Color.BLACK)
+            .setColorized(true)
+            .setStyle(Notification.DecoratedCustomViewStyle())
+            .setSmallIcon(R.drawable.ic_notification_alarm)
             .setContentIntent(pendingIntent)
             .setContentText(body)
             .setAutoCancel(true)
