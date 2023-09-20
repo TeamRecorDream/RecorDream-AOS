5ee8614948a882f8cc5b1ca1a27efcf7136d9433
yoon <cbj0010@naver.com>
2023-09-20 20:21:17 +0900

    [Chore] 주석삭제

--- app/src/main/java/com/team/recordream/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/team/recordream/presentation/home/HomeFragment.kt
@@ -1,7 +1,6 @@
 package com.team.recordream.presentation.home
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.RecyclerView
